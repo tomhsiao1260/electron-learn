90612c7962e9dab693d752830314c6bb4b637129
Hsiao Yao <tomtogo1260@gmail.com>
2024-02-03 01:54:07 +0800

    lesson 02

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const { app, BrowserWindow } = require('electron')
 const url = require('url')
 const path = require('path')
 
+// create a new react app in ./app folder and then build it
+// once finish, npm start to run this electron app
+// vite.config.js need to add base: '' option to make its path build correctly
+
 function createMainWindow() {
     const mainWindow = new BrowserWindow({ title: 'Electron', width: 1000, height: 600 })
-    const starturl = url.format({ pathname: path.join(__dirname, 'index.html'), protocol: 'file' })
+    const starturl = url.format({ pathname: path.join(__dirname, './app/dist/index.html'), protocol: 'file' })
     mainWindow.loadURL(starturl)
 }
 
--- readme.md
+++ readme.md
@@ -1,7 +1,3 @@
 # Introduction
 
-A quick learn of Electron from here
-https://www.youtube.com/watch?v=WZmevodgNEA&list=PLkZU2rKh1mT8cML-VNcUHF3vB8qzzgxuA
-
-Final Code:
-https://my-lnk.com/3186169894
+A quick learn of Electron from [here](https://www.youtube.com/watch?v=WZmevodgNEA&list=PLkZU2rKh1mT8cML-VNcUHF3vB8qzzgxuA) and its [final code](https://my-lnk.com/3186169894)
